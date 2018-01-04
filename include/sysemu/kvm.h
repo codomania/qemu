f1be643f30ff452698351f12da5445cee4b9b2f5
Brijesh Singh <brijesh.singh@amd.com>
2018-01-04 11:22:49 -0600

    kvm: introduce memory encryption APIs
    Inorder to integerate the Secure Encryption Virtualization (SEV) support
    add few high-level memory encryption APIs which can be used for encrypting
    the guest memory region.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: kvm@vger.kernel.org
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- include/sysemu/kvm.h
+++ include/sysemu/kvm.h
@@ -231,6 +231,33 @@ int kvm_destroy_vcpu(CPUState *cpu);
  */
 bool kvm_arm_supports_user_irq(void);
 
+/**
+ * kvm_memcrypt_enabled - return boolean indicating whether memory encryption
+ *                        is enabled
+ * Returns: 1 memory encryption is enabled
+ *          0 memory encryption is disabled
+ */
+bool kvm_memcrypt_enabled(void);
+
+/**
+ * kvm_memcrypt_encrypt_data: encrypt the memory range
+ *
+ * Return: 1 failed to encrypt the range
+ *         0 succesfully encrypted memory region
+ */
+int kvm_memcrypt_encrypt_data(uint8_t *ptr, uint64_t len);
+
+/**
+ * kvm_memcrypt_set_memory_region: indicate the memory region contains
+ *                                 encrypted data.
+ *
+ * When memory region is marked as encrypted then debug access will use
+ * encryption APIs and similarly migration will use call the encrypted
+ * APIs.
+ */
+void kvm_memcrypt_set_memory_region(MemoryRegion *mr);
+
+
 #ifdef NEED_CPU_H
 #include "cpu.h"
 
