0c579b2a0d4fca7dd8879a14a1dd38fd1a27ebc9
Brijesh Singh <brijesh.singh@amd.com>
2018-01-04 11:22:50 -0600

    sev: add debug encrypt and decrypt commands
    KVM_SEV_DBG_DECRYPT and KVM_SEV_DBG_ENCRYPT commands are used for
    decrypting and encrypting guest memory region. The command works only if
    the guest policy allows the debugging.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: kvm@vger.kernel.org
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -1677,6 +1677,7 @@ static int kvm_init(MachineState *ms)
         }
 
         kvm_state->memcrypt_encrypt_data = sev_encrypt_data;
+        kvm_state->memcrypt_set_memory_region = sev_set_memory_region;
     }
 
     ret = kvm_arch_init(ms, s);
